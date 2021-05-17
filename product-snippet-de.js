4532639b54ccc7ba8b85261386b04e57d93c1c71
Yura-Sokolovskyi <yuraprmm@gmail.com>
2021-05-17 16:36:42 +0300

    1

--- product-snippet-de.js
+++ product-snippet-de.js
@@ -1,1 +1,16 @@
-alert('test11')
+const bodyNode = document.querySelector('body')
+
+
+const snippetMainContainer = document.createElement('div')
+const snippetControlContainer = document.createElement('div')
+const snippetPageView = document.createElement('iframe')
+
+
+
+
+snippetPageView.setAttribute('src', 'https://dev-74941-petcare-purinattt-germany.pantheonsite.io/admin/content')
+
+snippetMainContainer.append(snippetControlContainer)
+snippetMainContainer.append(snippetPageView)
+
+bodyNode.append(snippetMainContainer)
